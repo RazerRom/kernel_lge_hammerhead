f9c5ae570f9223f36ed854852e46522c21941560
flar2 <asegaert@gmail.com>
2013-11-20 21:36:09 -0500

    Option to disable touch wake with power button
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -33,7 +33,9 @@
 #include <linux/input/doubletap2wake.h>
 #endif
 #endif
-
+#ifdef CONFIG_PWRKEY_SUSPEND
+#include <linux/qpnp/power-on.h>
+#endif
 #include "mdss_dsi.h"
 
 #include <asm/system_info.h>
@@ -200,6 +202,11 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
 #endif
+#ifdef CONFIG_PWRKEY_SUSPEND
+	if (pwrkey_pressed)
+		prevent_sleep = false;
+#endif
+
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
 		return;
@@ -333,6 +340,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &local_pdata->on_cmds);
 	mutex_unlock(&panel_cmd_mutex);
 
+#ifdef CONFIG_PWRKEY_SUSPEND
+	pwrkey_pressed = false;	
+#endif
+		
 	pr_info("%s\n", __func__);
 	return 0;
 }
@@ -352,6 +363,11 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
 #endif
+#ifdef CONFIG_PWRKEY_SUSPEND
+	if (pwrkey_pressed)
+		prevent_sleep = false;
+#endif
+
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
