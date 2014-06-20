36e3c19cb38b7af84e767818cc073efd20d019fc
Paul Reioux <reioux@gmail.com>
2014-06-20 12:55:32 -0500

    drivers/video/msm/mdss/mdss_dsi_panel.c: update powersuspend hook calls
    fix typo!
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -293,7 +293,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	}
 
 #ifdef CONFIG_POWERSUSPEND
-	set_power_suspend_state_pannel_hook(POWER_SUSPEND_INACTIVE);
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
 
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -337,7 +337,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	mutex_unlock(&panel_cmd_mutex);
 
 #ifdef CONFIG_POWERSUSPEND
-	set_power_suspend_state_pannel_hook(POWER_SUSPEND_ACTIVE);
+	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
 
 	pr_info("%s:\n", __func__);
