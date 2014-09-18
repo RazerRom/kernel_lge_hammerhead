47fdbf1c121efca542aa94a8390decece67baee1
rachanta <ravikumar.achanta@gmail.com>
2014-09-18 09:38:58 +0530

    Linux 3.4.92: Merge tag 'v3.4.92' into test
    This is the 3.4.92 stable release
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	include/net/ip6_route.h
    	kernel/futex.c
    
    Conflicts:
    	kernel/futex.c

--- kernel/futex.c
+++ kernel/futex.c
@@ -669,6 +669,7 @@ lookup_pi_state(u32 uval, struct futex_hash_bucket *hb,
 			 * Handle the owner died case:
 			 */
 			if (uval & FUTEX_OWNER_DIED) {
+
 				/*
 				 * exit_pi_state_list sets owner to NULL and
 				 * wakes the topmost waiter. The task which
@@ -699,6 +700,7 @@ lookup_pi_state(u32 uval, struct futex_hash_bucket *hb,
 				 *
 				 * Take a ref on the state and return. [6]
 				 */
+
 				if (!pid)
 					goto out_state;
 			} else {
@@ -1269,10 +1271,7 @@ void requeue_pi_wake_futex(struct futex_q *q, union futex_key *key,
  * Wake the top waiter if we succeed.  If the caller specified set_waiters,
  * then direct futex_lock_pi_atomic() to force setting the FUTEX_WAITERS bit.
  * hb1 and hb2 must be held by the caller.
- *
- * Returns:
- *  0 - failed to acquire the lock atomicly
- *  1 - acquired the lock
+
  * <0 - error
  */
 static int futex_proxy_trylock_atomic(u32 __user *pifutex,
@@ -1455,10 +1454,21 @@ retry_private:
 			WARN_ON(pi_state);
 			drop_count++;
 			task_count++;
-			ret = get_futex_value_locked(&curval2, uaddr2);
-			if (!ret)
-				ret = lookup_pi_state(curval2, hb2, &key2,
-						      &pi_state);
+
+			/*
+			 * If we acquired the lock, then the user
+			 * space value of uaddr2 should be vpid. It
+			 * cannot be changed by the top waiter as it
+			 * is blocked on hb2 lock if it tries to do
+			 * so. If something fiddled with it behind our
+			 * back the pi state lookup might unearth
+			 * it. So we rather use the known value than
+			 * rereading and handing potential crap to
+			 * lookup_pi_state.
+			 */
+
+			ret = lookup_pi_state(ret, hb2, &key2, &pi_state);
+
 		}
 
 		switch (ret) {
