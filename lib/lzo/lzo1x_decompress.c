95fb4d2fbeebcadddc2bc0f471864df837612a5b
rachanta <ravikumar.achanta@gmail.com>
2014-11-18 21:14:33 +0530

    lzo: remove duplicate definition of COPY4 to fix build warning

--- lib/lzo/lzo1x_decompress.c
+++ lib/lzo/lzo1x_decompress.c
@@ -24,8 +24,6 @@
 #define HAVE_OP(x, op_end, op) ((size_t)(op_end - op) < (x))
 #define HAVE_LB(m_pos, out, op) (m_pos < out || m_pos >= op)
 
-#define COPY4(dst, src)	\
-		put_unaligned(get_unaligned((const u32 *)(src)), (u32 *)(dst))
 
 int lzo1x_decompress_safe(const unsigned char *in, size_t in_len,
 			unsigned char *out, size_t *out_len)
