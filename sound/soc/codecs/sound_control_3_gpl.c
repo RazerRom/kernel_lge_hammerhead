677ff21f9e680df0bb56ca67f6e61929658a8786
engstk <eng.stk@sapo.pt>
2014-12-01 16:32:22 +0000

    sound: Hack Faux sound
    Thanks to @myfluxi for the idea
    This hack will always return true for calc_checksum()
    which is needed to control sound registers via
    terminal commands!
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -184,6 +184,7 @@ EXPORT_SYMBOL(snd_hax_reg_access);
 
 static bool calc_checksum(unsigned int a, unsigned int b, unsigned int c)
 {
+	/* 
 	unsigned char chksum = 0;
 
 	chksum = ~((a & 0xff) + (b & 0xff));
@@ -193,6 +194,8 @@ static bool calc_checksum(unsigned int a, unsigned int b, unsigned int c)
 	} else {
 		return false;
 	}
+	*/
+	return true; // Hack to always return true
 }
 
 static ssize_t cam_mic_gain_show(struct kobject *kobj,
