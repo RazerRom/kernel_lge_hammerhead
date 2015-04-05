a15e3ae6e2345d7dc8a8c046fbea3ce1e149652f
João Paulo <joaolrpaulo@gmail.com>
2015-04-05 14:47:32 +0100

    Revert "LCD_KCAL: remove the control bit, I hate it. Also clean up the code a little bit."
    This reverts commit 803cf89f582714a8a1020fdf8eec343d802df198.

--- arch/arm/mach-msm/lge/board-8974-hammerhead.c
+++ arch/arm/mach-msm/lge/board-8974-hammerhead.c
@@ -95,23 +95,15 @@ static void __init msm8974_early_memory(void)
 }
 
 #if defined(CONFIG_LCD_KCAL)
-int g_kcal_r;
-int g_kcal_g;
-int g_kcal_b;
+extern int g_kcal_r;
+extern int g_kcal_g;
+extern int g_kcal_b;
 
 int kcal_set_values(int kcal_r, int kcal_g, int kcal_b)
 {
-	if (kcal_r < 0 || kcal_r > 255)
-		return 1;
-	if (kcal_g < 0 || kcal_g > 255)
-		return 1;
-	if (kcal_b < 0 || kcal_b > 255)
-		return 1;
-
 	g_kcal_r = kcal_r;
 	g_kcal_g = kcal_g;
 	g_kcal_b = kcal_b;
-
 	return 0;
 }
 
