357039337b28e15476bd3fcceee4e5006327b0ba
KangDroid <hyunwoo286@gmail.com>
2015-05-11 20:09:17 +0100

    Fix build error
    Change-Id: I6fb08f325d71b1f624cc3d009a1d6f86b394fbf9
    Signed-off-by: gamerman123x <19superguy98@gmail.com>
    
    Conflicts:
    	drivers/usb/dwc3/gadget.c

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2015,9 +2015,6 @@ static int dwc3_cleanup_done_reqs(struct dwc3 *dwc, struct dwc3_ep *dep,
 			} else {
 				dep->flags &= ~DWC3_EP_MISSED_ISOC;
 			}
-
-			if (last_one)
-				break;
 		} else {
 			if (count && (event->status & DEPEVT_STATUS_SHORT))
 				s_pkt = 1;
--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1370,8 +1370,6 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
-	unsigned long atomic_flags; /* Flags needing atomic access. */
-
 	pid_t pid;
 	pid_t tgid;
 
