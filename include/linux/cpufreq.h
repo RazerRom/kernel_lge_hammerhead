b2095d7cabe02489464a1743a46d5ef98e07e9d3
gamerman123x <19superguy98@gmail.com>
2015-05-13 23:10:36 -0400

    Add Uberdemand governor
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -414,6 +414,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UBERDEMAND)
+extern struct cpufreq_governor cpufreq_gov_uberdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_uberdemand)
 #endif
 
 
